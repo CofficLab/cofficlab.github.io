ce190a632c91c029b3b5fccbb89acb16a2ab790d
Nookery <nooks@qq.com>
2024-12-17 18:26:02 +0800

    💄 UI:  Update user interface elements

--- docs/components/FeatureGroup.vue
+++ docs/components/FeatureGroup.vue
@@ -12,15 +12,15 @@ export default {
     methods: {
         getBackgroundClass(index) {
             const bgClasses = [
-                'bg-gradient-to-b from-blue-50  to-blue-200',
-                'bg-gradient-to-b from-blue-200  to-purple-200',
-                'bg-gradient-to-b from-purple-200  to-pink-200',
-                'bg-gradient-to-b from-pink-200  to-red-200',
-                'bg-gradient-to-b from-red-200  to-orange-200',
-                'bg-gradient-to-b from-orange-200 to-yellow-200',
-                'bg-gradient-to-b from-yellow-200  to-green-200',
-                'bg-gradient-to-b from-green-200  to-teal-200',
-                'bg-gradient-to-b from-teal-200  to-blue-200',
+                'bg-gradient-to-b from-blue-50/50  to-blue-200/50',
+                'bg-gradient-to-b from-blue-200/50  to-purple-200/50',
+                'bg-gradient-to-b from-purple-200/50  to-pink-200/50',
+                'bg-gradient-to-b from-pink-200/50  to-red-200/50',
+                'bg-gradient-to-b from-red-200/50  to-orange-200/50',
+                'bg-gradient-to-b from-orange-200/50  to-yellow-200/50',
+                'bg-gradient-to-b from-yellow-200/50  to-green-200/50',
+                'bg-gradient-to-b from-green-200/50  to-teal-200/50',
+                'bg-gradient-to-b from-teal-200/50  to-blue-200/50',
             ];
             return bgClasses[index % bgClasses.length];
         }
