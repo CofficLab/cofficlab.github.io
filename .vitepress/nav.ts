a5aa0c50179c6981de16f7fff537701048097578
Nookery <nooks@qq.com>
2024-12-18 15:17:01 +0800

    🐛 Bugfix:  Fix a bug

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -1,7 +1,7 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const createNav = (lang: string): DefaultTheme.NavItem[] => [
-    { text: 'Home', link: `/${lang}` },
+    { text: 'Home', link: `/${lang}/` },
     { text: 'JuiceEditor', link: `/${lang}/juiceEditor` },
     { text: lang === 'zh' ? 'Cisum-乐音' : 'Cisum', link: `/${lang}/cisum` },
     { text: lang === 'zh' ? '聚思笔记' : 'JuiceNote', link: `/${lang}/juiceNote` },
