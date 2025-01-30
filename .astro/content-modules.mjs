20b873016e781bd78fd6e3ea859599e018007eee
Nookery <nooks@qq.com>
2025-01-30 22:06:49 +0800

    🎨 Chore:  Minor adjustments

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,1 @@
-
-export default new Map([
-["src/content/docs/juiceNote.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2FjuiceNote.mdx&astroContentModuleFlag=true")],
-["src/content/docs/travelmode.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftravelmode.mdx&astroContentModuleFlag=true")],
-["src/content/docs/cisum.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcisum.mdx&astroContentModuleFlag=true")],
-["src/content/docs/juiceEditor.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2FjuiceEditor.mdx&astroContentModuleFlag=true")]]);
-		
+export default new Map();
--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cofficlab.github.io',
   integrations: [
     starlight({
       title: 'Coffic',
