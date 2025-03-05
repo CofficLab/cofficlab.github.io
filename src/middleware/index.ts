8cac5b674bcdf66b260335b767475881d49fc5fe
Nookery <nooks@qq.com>
2025-03-05 12:10:49 +0800

    🐎 Improve:  Enhance existing functionality

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,6 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((context, next) => {
-  return next();
+  // 将所有请求重定向到目标网站
+  return Response.redirect('https://cofficlab.pages.dev', 301);
 });
